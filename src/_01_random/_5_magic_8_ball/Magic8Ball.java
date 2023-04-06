a7b4de36743d2639245fddd22e60b0a762cad9a3
League <zach.dantsker@gmail.com>
2023-04-06 16:58:03 -0700

    finished validation not done with 8 ball

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,20 +6,36 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
-public class Magic8Ball {
+import javax.swing.JOptionPane;
 
+public class Magic8Ball {
+	public static void main(String[] args) {
 	// 1. Make a main method that includes all the steps below
 
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+		String x = JOptionPane.showInputDialog("Enter a question for the 8 Ball to answer: ");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
+		Random randomMaker = new Random();
+		
+		int ran = randomMaker.nextInt(4);
+		
 	
 	// 4. If the random number is 0
-
+		if (ran == 0) {
+			System.out.println("Yeah, probably.");
+		}
+		else if (ran == 1) {
+			System.out.println("Outlook not so good.");
+		}
+		else if (ran == 2) {
+			System.out.println("Maybe ask Google?");
+		}
+		else {
+			System.out.println("My mind is not concetrating right now. Pls try again.");
+		}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
@@ -33,5 +49,5 @@ public class Magic8Ball {
 	// 7. If the random number is 3
 
 	// -- write your own answer
-
+	}
 }
